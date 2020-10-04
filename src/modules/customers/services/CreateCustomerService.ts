425e116135f19a1337ed17435c2de56e6f69a711
otaviopetry <otaviopetry@gmail.com>
2020-10-04 20:06:31 -0300

    feat: Register new customer
    Create customer working

--- src/modules/customers/services/CreateCustomerService.ts
+++ src/modules/customers/services/CreateCustomerService.ts
@@ -12,10 +12,25 @@ interface IRequest {
 
 @injectable()
 class CreateCustomerService {
-  constructor(private customersRepository: ICustomersRepository) {}
+  constructor(
+    @inject('CustomersRepository')
+    private customersRepository: ICustomersRepository,
+  ) {}
 
   public async execute({ name, email }: IRequest): Promise<Customer> {
     // TODO
+    const userExists = await this.customersRepository.findByEmail(email);
+
+    if (userExists) {
+      throw new AppError('The informed email address is already in use.');
+    }
+
+    const customer = await this.customersRepository.create({
+      name,
+      email,
+    });
+
+    return customer;
   }
 }
 
